9151019c50e46c1eccfe801ee59b1df9729ddd14
nayebare <mnayebare@rwanda.cmu.edu>
2019-11-02 16:26:51 +0300

    added details

--- public/js/app.js
+++ public/js/app.js
@@ -16,27 +16,21 @@ $(document).ready(function () {
       alert("The age field can not be empty.Please enter the valid age.");
     }
 
-    //apply mock-backend server result here
     var markup = "<tr style='background-color:#ffe484;'><td>" + fname + '</td><td>' + lname + '</td><td>' + age + '</td><td>' + sex + '</td><td>' + country + '</td><td>' + city + '</td><td>' + diabetesCheck + '</td><td><span class="deletebtn">x</span></td></tr>';
     $('table tbody').append(markup);
-  });// end main function
+    });// end main function
 
-  //trying the filter function
   $("#minors_check").click(function () {
     table = document.getElementById("customerDetails");
     tr = table.getElementsByTagName("tr");
-
+  
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[0];
-      alert(tr[i].getElementsByTagName("td")[0])
-
       if (td) {
         txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+        if (txtValue > 18) {
+          //remove table row
         }
       }
     }
