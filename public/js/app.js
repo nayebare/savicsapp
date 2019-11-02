7b96504ddec4c1cda36d8a5979f177307a3150ba
nayebare <mnayebare@rwanda.cmu.edu>
2019-11-02 16:17:11 +0300

    valid age

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,15 @@ $(document).ready(function () {
     var fname = $('#fname').val();
     lname = $('#lname').val();
     age = $('#age').val();
-    sex = $('input[name=gender]:checked', '#medicalForm').val(); 
+    sex = $('input[name=gender]:checked', '#medicalForm').val();
     city = $('#city').val();
     country = $('#country').val();
-    diabetesCheck = $('input[name=diabetesCheck]:checked', '#medicalForm').val(); 
+    diabetesCheck = $('input[name=diabetesCheck]:checked', '#medicalForm').val();
+
+
+    if (!/^[0-9]+$/.test(age) || age == '') {
+      alert("The age field can not be empty.Please enter the valid age.");
+    }
 
     //apply mock-backend server result here
     var markup = "<tr style='background-color:#ffe484;'><td>" + fname + '</td><td>' + lname + '</td><td>' + age + '</td><td>' + sex + '</td><td>' + country + '</td><td>' + city + '</td><td>' + diabetesCheck + '</td><td><span class="deletebtn">x</span></td></tr>';
@@ -17,21 +22,15 @@ $(document).ready(function () {
   });// end main function
 
   //trying the filter function
-
-  var tds = $("table#customerDetails tr td:contains('male')");
-
-
-  $("#minors_check").click(function() { 
-
+  $("#minors_check").click(function () {
     table = document.getElementById("customerDetails");
     tr = table.getElementsByTagName("tr");
-  
+
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-       
       td = tr[i].getElementsByTagName("td")[0];
       alert(tr[i].getElementsByTagName("td")[0])
-  
+
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -40,10 +39,10 @@ $(document).ready(function () {
           tr[i].style.display = "none";
         }
       }
-}
+    }
 
 
-}); 
+  });
   // Find and remove selected table rows
   $(document).on('click', 'span.deletebtn', function () {
     $(this).closest('tr').remove();
